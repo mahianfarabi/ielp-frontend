e1024f4417418a5c619d800ce9b4d1e4e476dc67
Mahian Farabi <mahianfarabi@gmail.com>
2025-11-10 11:58:46 +0600

    Adding frontend to git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/")
+      .then(res => setMessage(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return <h1>{message}</h1>;
 }
 
 export default App;
